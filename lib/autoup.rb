46180de8924c1631fe51f4cb15aeca2c4b89a23c
Vasily Korytov <vasily.korytov@yahoo.com>
2014-11-10 12:40:46 +0200

    move `browse` and `launch` methods out of the constructor

--- lib/autoup.rb
+++ lib/autoup.rb
@@ -23,7 +23,7 @@ require 'yaml'
 #
 # Offline copies of README and example config are also included in this gem.
 class AutoUp
-  # Launches AutoUp (specify the config file to override the default)
+  # Read config file and launch AutoUp (specify the config file to override the default).
   def initialize(yml = File.expand_path('~/.config/autoup.yml'))
     begin
       config = YAML.load_file(yml)
@@ -33,67 +33,77 @@ class AutoUp
       return
     end
     config.each { |cfg|
-      # Required settings
-      unless cfg['forum']
-        puts_required('forum')
-        next
-      end
-      unless cfg['user']
-        puts_required('user')
-        next
-      end
-      unless cfg['pass']
-        puts_required('pass')
-        next
-      end
-      # Optional settings
-      cfg['timeout'] ||= 5
-      cfg['find']    ||= 'Find all threads started by'
-      cfg['stats']   ||= 'Show All Statistics'
-      cfg['up']      ||= 'Bump the thread!'
+      launch(cfg)
+    }
+  end
 
-      web = Selenium::WebDriver.for :firefox
+  # Launch the AutoUp process.
+  def launch(cfg)
+    # Required settings
+    unless cfg['forum']
+      puts_required('forum')
+      return
+    end
+    unless cfg['user']
+      puts_required('user')
+      return
+    end
+    unless cfg['pass']
+      puts_required('pass')
+      return
+    end
+    # Optional settings
+    cfg['timeout'] ||= 5
+    cfg['find']    ||= 'Find all threads started by'
+    cfg['stats']   ||= 'Show All Statistics'
+    cfg['up']      ||= 'Bump the thread!'
+
+    web = Selenium::WebDriver.for :firefox
+    begin
+      browse(web, cfg)
+    ensure
+      web.quit
+    end
+  end
+
+  # Process a forum board.
+  def browse(web, cfg)
+    web.manage.timeouts.implicit_wait = cfg['timeout']
+    web.get(cfg['forum'])
+
+    begin
+      web.find_element(:name, 'vb_login_username').send_keys(cfg['user'])
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      puts("ERROR: unable to load #{cfg['forum']} (does not load or does not contain vb_login_username)")
+      return
+    end
+
+    login = web.find_element(:name, 'vb_login_password')
+    login.send_keys(cfg['pass'])
+    login.submit
+
+    begin
+      web.find_element(:link, cfg['user']).click
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      puts("ERROR: unable to login as #{cfg['user']} on #{cfg['forum']}")
+      return
+    end
+
+    web.find_element(:link, cfg['stats']).click
+    web.find_element(:link, "#{cfg['find']} #{cfg['user']}").click
+
+    links = []
+    web.find_elements(:css, 'a[id ^= "thread_title_"]').each { |link|
+      links.push link.attribute('href')
+    }
+
+    links.each { |link|
+      web.get(link)
       begin
-        web.manage.timeouts.implicit_wait = cfg['timeout']
-        web.get(cfg['forum'])
-
-        begin
-          web.find_element(:name, 'vb_login_username').send_keys(cfg['user'])
-        rescue Selenium::WebDriver::Error::NoSuchElementError
-          puts("ERROR: unable to load #{cfg['forum']} (does not load or does not contain vb_login_username)")
-          next
-        end
-
-        login = web.find_element(:name, 'vb_login_password')
-        login.send_keys(cfg['pass'])
-        login.submit
-
-        begin
-          web.find_element(:link, cfg['user']).click
-        rescue Selenium::WebDriver::Error::NoSuchElementError
-          puts("ERROR: unable to login as #{cfg['user']} on #{cfg['forum']}")
-          next
-        end
-
-        web.find_element(:link, cfg['stats']).click
-        web.find_element(:link, "#{cfg['find']} #{cfg['user']}").click
-
-        links = []
-        web.find_elements(:css, 'a[id ^= "thread_title_"]').each { |link|
-          links.push link.attribute('href')
-        }
-
-        links.each { |link|
-          web.get(link)
-          begin
-            web.find_element(:css, "input[value = '#{cfg['up']}']").click
-            puts("UP: #{link}")
-          rescue Selenium::WebDriver::Error::NoSuchElementError
-            puts("SKIP: #{link}")
-          end
-        }
-      ensure
-        web.quit
+        web.find_element(:css, "input[value = '#{cfg['up']}']").click
+        puts("UP: #{link}")
+      rescue Selenium::WebDriver::Error::NoSuchElementError
+        puts("SKIP: #{link}")
       end
     }
   end
